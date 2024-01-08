f0328f929a02b25cc995d59947f59ba3b9950871
Jeremy Caldwell <jeremy@eternalistic.net>
2024-01-08 15:06:16 -0800

    Added dynamic copyright date to footer

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 	eleventyConfig.addGlobalData('currentYear', '2024');
+  eleventyConfig.addShortcode('year', () => {
+    return `${new Date().getFullYear()}`;
+  });
   [
     'src/css',
     'src/images/',
